2e3c9af170ba323ed8948745dc34ba0a5e60f739
TovarichPilot <ilyas.gareev.1999@mail.ru>
2021-09-02 17:44:52 +0400

    Wild-Cars
    Control a car,smash objects and invite your friend to find out who is faster.

---
+++ Jump-Game/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+
+    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 frontOffset = new Vector3(0, 1.92f, 0.4f);
+
+    public Camera frontCameraPlayer1;
+    public Camera frontCameraPlayer2;
+
+    public Camera mainCameraPlayer1;
+    public Camera mainCameraPlayer2;
+
+    public int CamModePlayer1 = 0;
+    public int CamModePlayer2 = 0;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // offset the camera behind the player be adding to the player's position 
+        mainCameraPlayer1.transform.position = player1.transform.position + offset;
+        mainCameraPlayer2.transform.position = player2.transform.position + offset;
+        // offset the front camera behind the player
+        frontCameraPlayer1.transform.position = player1.transform.position + frontOffset;
+        frontCameraPlayer2.transform.position = player2.transform.position + frontOffset;
+
+        // change player1 view pressing C button
+        if (Input.GetButtonDown("CameraPlayer1"))
+        {
+            if (CamModePlayer1 == 1)
+            {
+                CamModePlayer1 = 0;
+            }
+            else
+            {
+                CamModePlayer1 += 1;
+            }
+
+            StartCoroutine(CameraChangePlayer1());
+        }
+
+        // change player2 view pressing M button
+        if (Input.GetButtonDown("CameraPlayer2"))
+        {
+            if (CamModePlayer2 == 1)
+            {
+                CamModePlayer2 = 0;
+            }
+            else
+            {
+                CamModePlayer2 += 1;
+            }
+
+            StartCoroutine(CameraChangePlayer2());
+        }
+    }
+
+    IEnumerator CameraChangePlayer1()
+    {
+        yield return new WaitForSeconds(0.01f);
+
+        if (CamModePlayer1 == 0)
+        {
+            mainCameraPlayer1.gameObject.SetActive(true);
+            frontCameraPlayer1.gameObject.SetActive(false);
+        }
+
+        if (CamModePlayer1 == 1)
+        {
+            mainCameraPlayer1.gameObject.SetActive(false);
+            frontCameraPlayer1.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator CameraChangePlayer2()
+    {
+        yield return new WaitForSeconds(0.01f);
+
+        if (CamModePlayer2 == 0)
+        {
+            mainCameraPlayer2.gameObject.SetActive(true);
+            frontCameraPlayer2.gameObject.SetActive(false);
+        }
+        if (CamModePlayer2 == 1)
+        {
+            mainCameraPlayer2.gameObject.SetActive(false);
+            frontCameraPlayer2.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Jump-Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Player1, Player2;
+    private float speed = 13.0f;
+    private float turnSpeed = 40.0f;
+    private float horizontalInputPlayer1, horizontalInputPlayer2;
+    private float forwardInputPlayer1, forwardInputPlayer2;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is where we get player input
+        horizontalInputPlayer1 = Input.GetAxis("Horizontal");
+        forwardInputPlayer1 = Input.GetAxis("Vertical");
+
+        horizontalInputPlayer2 = Input.GetAxis("Horizontal2");
+        forwardInputPlayer2 = Input.GetAxis("Vertical2");
+
+        // we  move the player1 and player2 forward
+        Player1.transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInputPlayer1);
+        Player2.transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInputPlayer2);
+
+        // backword translation player1
+        if (forwardInputPlayer1 < 0) Player1.transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * (-horizontalInputPlayer1));
+        else Player1.transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInputPlayer1);
+
+        // backword translation player2
+        if (forwardInputPlayer2 < 0) Player2.transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * (-horizontalInputPlayer2));
+        else Player2.transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInputPlayer2);
+
+    }
+}
